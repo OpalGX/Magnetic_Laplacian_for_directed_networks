f83f9fa93c2696e37a60b23e93ee2ed5053a2b09
OpalGX <43104091+OpalGX@users.noreply.github.com>
2020-10-13 12:06:40 +0800

    Update meigenmaps.m

--- functions/meigenmaps.m
+++ functions/meigenmaps.m
@@ -1,4 +1,9 @@
-function [G, eig, p, V, D] = meigenmaps(G,g)
+function [G, eig, p, Phi, D] = meigenmaps(G,g)
+%G plot the nodes against the eigenvalue
+%eig is the top eigenvector
+%p is the network plot
+%Phi are the phases corresponding to the top eigenvector
+%D is the top eigenvalue
 
     W = adjacency(G);
     % Symmetric weights.
@@ -11,6 +16,16 @@ function [G, eig, p, V, D] = meigenmaps(G,g)
     deg = sum(Ws,2); Deg = diag(deg); % Degree matrix.?
     Tg = exp(1) .^(2*pi*1i*g*A.');  % Transporter
     Lg = Deg - Ws.*Tg; % Magnetic Laplacian.
+    %Lg = inv(sqrtm(Deg))*Lg*inv(sqrtm(Deg));% Normalized Laplacian.
+    [V,D] = eigs(Lg,size(W,1),'smallestabs'); % All eigenvectors ranging from smallest eigenvalue to largest eigenvalue
+    D = diag(D);
+    Phi = angle(V); %phases corresponding to all eigenvectors
+ 
+    G.Nodes.phase0 = mod(Phi(:,1), 2*pi);%phases corresponding to the top eigenvector
+    G.Nodes.phase1 = mod(Phi(:,2), 2*pi);
+    
+    %p = plot(G,'XData', G.Nodes.phase0 ,'YData',G.Nodes.phase1 , 'NodeLabel', G.Nodes.Name);
+    p = plot(G,'XData', cos(G.Nodes.phase0) ,'YData', sin(G.Nodes.phase0)); %plot on polar coordinate
     Lng = inv(sqrtm(Deg))*Lg*inv(sqrtm(Deg));% Normalized Laplacian.
     [V,D] = eigs(Lng,size(W,1),'smallestabs'); % Eigenvectors ranging from smallest
     Phi = mod(angle(V),2*pi); %Phases.
@@ -24,8 +39,5 @@ function [G, eig, p, V, D] = meigenmaps(G,g)
     p.NodeColor = 'r';
     xlabel('phase(\phi_0)');
     ylabel('phase(\phi_1)');
-    
-    eig = round(D(1,1),4);
-    
-
+    eig = round(D(1,1),4); %top eigenvalue
 end
